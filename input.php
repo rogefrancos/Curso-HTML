20b4694254a4c627c57ff2e02396d09acf68a6e9
rogefrancos <rogefs04@gmail.com>
2024-05-29 00:51:21 -0600

    Add files via upload

--- input.php
+++ input.php
@@ -1,10 +1,22 @@
 <?php
 
-if(isset($_GET['foodmc1'])){
-
-echo"puras mamadas";
-
+session_start();
+if (!isset($_SESSION['carrito'])) {
+    $_SESSION['carrito'] = array();
 }
 
+if (isset($_POST['add_to_cart'])) {
+    $product_id = $_POST['product_id']; // Obtén el identificador del producto
 
+    // Ejemplo: Agregar el producto al carrito
+    if (!isset($_SESSION['carrito'][$product_id])) {
+        $_SESSION['carrito'][$product_id] = 1; // Inicializa la cantidad en 1
+    } else {
+        $_SESSION['carrito'][$product_id]++; // Incrementa la cantidad si ya existe
+    }
+
+    // Redirige al usuario a la página de checkout
+    header('Location: checkout.html'); // Cambia la URL según tu estructura de archivos
+    exit; // Detiene la ejecución del script
+}
 ?>
